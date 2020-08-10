25f5a598f0e96297f27cf5d292ec6bee079f1907
aman <vyasaman21@gmail.com>
2020-08-10 18:57:28 +0100

    Updates

---
+++ precache-manifest.565a5fedd5d6541a38e59317108bd94f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fd748a84f8b9d0930bcc80137b315b",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "742274ce1633e4bf2c52",
+    "url": "/React/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "675839667b3e936d2a51",
+    "url": "/React/static/css/main.fee318a5.chunk.css"
+  },
+  {
+    "revision": "742274ce1633e4bf2c52",
+    "url": "/React/static/js/2.e448f2eb.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/React/static/js/2.e448f2eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675839667b3e936d2a51",
+    "url": "/React/static/js/main.e1fbf8ff.chunk.js"
+  },
+  {
+    "revision": "ef6dc424969cd6d09bd1",
+    "url": "/React/static/js/runtime-main.f79c6341.js"
+  },
+  {
+    "revision": "ccc4d1e9788404ce5a233f4df657d247",
+    "url": "/React/static/media/aman.ccc4d1e9.jpeg"
+  },
+  {
+    "revision": "632932ee686b17fb8e72aa6ecd6e8592",
+    "url": "/React/static/media/meta.632932ee.jpg"
+  },
+  {
+    "revision": "17552ffa2568eb2d8157fafd0e925465",
+    "url": "/React/static/media/naniredddy.17552ffa.jpeg"
+  },
+  {
+    "revision": "cbe6df28acde742549ed234b4e032c08",
+    "url": "/React/static/media/wifi.cbe6df28.jpg"
+  }
+]);
